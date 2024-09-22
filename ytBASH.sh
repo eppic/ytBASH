e796549afc8053432e1b57a9e64a62397d87e89f
eppic <42480669+eppic@users.noreply.github.com>
2024-09-23 01:05:42 +0200

    various additions and changes

--- ytBASH.sh
+++ ytBASH.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # version
-version=0.1
+version=0.2
 
 # menu 
 menu_main() {
@@ -11,6 +11,21 @@ menu_main() {
     switch_options
     print_details
 
+    # show if URL is invalid
+    if [ "$inv_url" = true ]; then
+        random_index=$((RANDOM % ${#inv_text[@]}))
+        random_text="${inv_text[$random_index]}"
+        
+        echo -e "\e[31m$random_text\e[0m"
+
+        inv_url=false
+    elif [ "$inv_url" = url ]; then
+        echo -e "\e[31mYou need to paste a Link (URL) from your web browser into this field\e[0m"
+        inv_url=false
+    elif [ "$inv_url" = short ]; then
+        inv_url=false
+    fi
+
     echo -n "Enter P/D/X/URL: "
     read URL
 
@@ -19,25 +34,31 @@ menu_main() {
         menu_preferences
         return
     elif [ "$URL" = "D" ] || [ "$URL" = "d" ]; then
-        xdg-open "$DOWNLOAD_DIR"
+        nohup xdg-open "$DOWNLOAD_DIR" &>/dev/null & 
         return
     elif [ "$URL" = "Q" ] || [ "$URL" = "q" ]; then
         # queue mode here
         return
     elif [ "$URL" = "H" ] || [ "$URL" = "h" ]; then
-        xdg-open ~/ytBASH-history
+        nohup xdg-open ~/.ytBASH-history &>/dev/null & 
+        return
+    elif [ "$URL" = "URL" ] || [ "$URL" = "url" ]; then
+        inv_url=url
         return
     elif [ "$URL" = "X" ] || [ "$URL" = "x" ]; then
         exit
-    elif [ -z "$URL" ] || [ "$URL" = "B" ] || [ "$URL" = "b" ] ; then
-        return
     fi
 
     # check cookie option before entering options menu
-    if [ "$cookiesdefault" = true ]; then
+    if [ "$cookiesdefault" = false ]; then
         option_cookies=true
     fi
 
+    check_url
+    if [ ! "$inv_url" = false ]; then
+        return
+    fi
+    clean_url
     menu_options 
     
 }
@@ -132,7 +153,7 @@ menu_options() {
             return
             ;;
         *)
-            invarg=true
+            inv_argument=true
             menu_options
             ;;
     esac
@@ -259,7 +280,12 @@ menu_preferences() {
     #        echo -e "\e[32m[TRUE]\e[0m"
     #    fi
     echo
-    echo "[G] Generate .desktop file "
+    if [ -f "$DESKTOP_FILE_PATH" ]; then
+        echo "[G] Delete .desktop file"
+    else
+        echo "[G] Generate .desktop file"
+    fi
+    echo "[O] Open script directory"
     echo
     echo "[B] Go Back "
     check_invalid
@@ -313,11 +339,14 @@ menu_preferences() {
         [Gg])
             pref_desktop
             ;;
+        [Oo])
+            nohup xdg-open "$(dirname "$(realpath "$0")")" &>/dev/null & 
+            ;;
         [Bb])
             return
             ;;
         *)
-            invarg=true
+            inv_argument=true
             ;;
     esac
 
@@ -326,12 +355,12 @@ menu_preferences() {
 }
 
 write_preferences() {
-    echo "# ytBASH-config-file:" > ~/ytBASH-config
-    echo "DOWNLOAD_DIR=$DOWNLOAD_DIR" >> ~/ytBASH-config
-    echo "keephistory=$keephistory" >> ~/ytBASH-config
-    echo "cookiesdefault=$cookiesdefault" >> ~/ytBASH-config
-    echo "thumbnailaudiocover=$thumbnailaudiocover" >> ~/ytBASH-config
-    echo "cleanqueue=$cleanqueue" >> ~/ytBASH-config
+    echo "# .ytBASH-config-file:" > ~/.ytBASH-config
+    echo "DOWNLOAD_DIR=$DOWNLOAD_DIR" >> ~/.ytBASH-config
+    echo "keephistory=$keephistory" >> ~/.ytBASH-config
+    echo "cookiesdefault=$cookiesdefault" >> ~/.ytBASH-config
+    echo "thumbnailaudiocover=$thumbnailaudiocover" >> ~/.ytBASH-config
+    echo "cleanqueue=$cleanqueue" >> ~/.ytBASH-config
 
     load_preferences
 }
@@ -339,7 +368,10 @@ write_preferences() {
 pref_defaultdir() {
     print_details
     echo "Choose the folder you want to use with the file manager window that just opened in the background."
-    echo "(If no file manager opens up you don't have zenity installed - In that case edit it manually: ~/ytBASH-config)"
+    echo
+    echo "Warning: If no file manager opens up you do not have zenity installed"
+    echo "In that case you need to edit it manually: ~/.ytBASH-config)"
+    echo
 
     DOWNLOAD_DIR_temp=$(zenity --file-selection --directory --title="Select Default Download Directory")
 
@@ -353,23 +385,34 @@ pref_defaultdir() {
 
 pref_desktop() {
     print_details
-    DESKTOP_FILE_PATH="$HOME/.local/share/applications/ytBASH.desktop"
     mkdir -p "$HOME/.local/share/applications"
 
-    # write the .desktop file content
-    echo "[Desktop Entry]" > "$DESKTOP_FILE_PATH"
-    echo "Version=$version" >> "$DESKTOP_FILE_PATH"
-    echo "Name=ytBASH" >> "$DESKTOP_FILE_PATH"
-    echo "Comment=Download video or audio using yt-dlp" >> "$DESKTOP_FILE_PATH"
-    echo "Exec=$SCRIPT_PATH" >> "$DESKTOP_FILE_PATH"
-    echo "Icon=youtube-dl" >> "$DESKTOP_FILE_PATH"
-    echo "Terminal=true" >> "$DESKTOP_FILE_PATH"
-    echo "Type=Application" >> "$DESKTOP_FILE_PATH"
-    echo "Categories=Utility;Network;" >> "$DESKTOP_FILE_PATH"
+    # delete the desktop entry
+    if [ -f "$DESKTOP_FILE_PATH" ]; then
+        rm "$DESKTOP_FILE_PATH"
+        if [ $? -eq 0 ]; then
+            echo "Desktop file successfully removed: $DESKTOP_FILE_PATH"
+        else
+            echo "Failed to remove desktop file: $DESKTOP_FILE_PATH"
+        fi
+        echo 
+        read -n 1 -p "Press any button..."
+    else
+        # write the .desktop file content
+        echo "[Desktop Entry]" > "$DESKTOP_FILE_PATH"
+        echo "Version=$version" >> "$DESKTOP_FILE_PATH"
+        echo "Name=ytBASH" >> "$DESKTOP_FILE_PATH"
+        echo "Comment=Download video or audio using yt-dlp" >> "$DESKTOP_FILE_PATH"
+        echo "Exec=$SCRIPT_PATH" >> "$DESKTOP_FILE_PATH"
+        echo "Icon=youtube-dl" >> "$DESKTOP_FILE_PATH"
+        echo "Terminal=true" >> "$DESKTOP_FILE_PATH"
+        echo "Type=Application" >> "$DESKTOP_FILE_PATH"
+        echo "Categories=Utility;Network;" >> "$DESKTOP_FILE_PATH"
 
-    echo "Desktop file generated at $DESKTOP_FILE_PATH"
-    echo
-    read -n 1 -p "Press any button..."
+        echo "Desktop file generated at $DESKTOP_FILE_PATH"
+        echo
+        read -n 1 -p "Press any button..."
+    fi
 }
 
 # various
@@ -402,18 +445,18 @@ print_details() {
 }
 
 check_invalid() {
-    if [ "$invarg" = true ]; then
+    if [ "$inv_argument" = true ]; then
         echo
         echo -e "\e[31mInvalid Argument!\e[0m"
     else 
         echo
     fi
-    invarg=false
+    inv_argument=false
 }
 
 check_keephistory() {
     if [ "$keephistory" = true ]; then
-        echo $URL >> ytBASH-history
+        echo $URL >> .ytBASH-history
     fi
 }
 
@@ -467,10 +510,29 @@ check_cookies() {
 
 switch_options() {
 
-    #set switch options back to default values
+    # set switch options back to default values
     option_playlist=false
     option_subtitles=false
     option_cookies=false
+}
+
+clean_url() {
+    if [[ "$URL" =~ ^\".*\"$ || "$URL" =~ ^\'.*\'$ ]]; then
+        URL="${URL:1:-1}"
+    fi
+}
+
+check_url() {
+    if [ "${#URL}" -le 4 ]; then
+        inv_url=short
+        return
+    elif [[ "$URL" =~ ^(https?://)?[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(/.*)?$ ]]; then
+        inv_url=false
+        return
+    else
+        inv_url=true
+        return
+    fi
 }
 
 # important
@@ -488,22 +550,42 @@ set_defaults() {
 
     # set default values
     URL=
-    invarg=false
+    inv_argument=false
     context=none
     cookiereturn=false
     option_cookies=false
     option_playlist=false
     option_subtitles=false
+    inv_url=false
+    DESKTOP_FILE_PATH="$HOME/.local/share/applications/ytBASH.desktop"
+
 
     switch_options    
 
     # script path as variable
     SCRIPT_PATH="$(realpath "$0")"
 
+    # invalid url text 
+    inv_text=(
+        "That's a weird URL."
+        "This doesn't look like a URL to me."
+        "Please check your URL."
+        "Your URL has one or more faults."
+        "Do you think that's a URL?"
+        "Weird URL detected."
+        "Faulty URL detected."
+        "Please stop spamming bad URLs."
+        "WARNING: CHECK YOUR URL IMMEDIATELY."
+        "This URL does not look right."
+        "Please eat your URL."
+        "Bad URL detected."
+        "There seems to be something wrong with your URL."
+        "That's not a valid URL."
+    )
 }
 
 load_preferences() {
-    if [ -f ~/ytBASH-config ]; then
+    if [ -f ~/.ytBASH-config ]; then
         while IFS='=' read -r key value; do
             case "$key" in
                 "DOWNLOAD_DIR") DOWNLOAD_DIR="$value" ;;
@@ -512,22 +594,48 @@ load_preferences() {
                 "thumbnailaudiocover") thumbnailaudiocover="$value" ;;
                 "cleanqueue") cleanqueue="$value" ;;
             esac
-        done < ~/ytBASH-config
+        done < ~/.ytBASH-config
     fi
 }
 
+check_dependencies() {
+    missing_software=()
+
+    # yt-dlp check
+    if ! command -v yt-dlp &> /dev/null; then
+        missing_software+=("yt-dlp")
+    fi
+
+    # ffmpeg check
+    if ! command -v ffmpeg &> /dev/null; then
+        missing_software+=("ffmpeg")
+    fi
+
+    # check if there is missing software
+    if [ ${#missing_software[@]} -eq 0 ]; then
+        return
+    else
+        print_details
+        echo "You are missing software required by ytBASH:"
+        echo
+        for software in "${missing_software[@]}"; do
+            echo "- $software"
+        done
+        echo
+        read -n 1 -p "Press any button..."
+    fi
+
+}
+
 # main script
-    set_defaults
-    load_preferences
-    while true; do 
-        menu_main
-    done
+check_dependencies
+set_defaults
+load_preferences
+while true; do 
+    menu_main
+done
 
 # todo: 
-    # queue
-    # dependency check
-    # check if url is already in "" or '' 
-    # check if url is a link
-    # say that it wrote to the history file
-    # custom arguments
-    # preferences shortcuts
+# queue
+# say that it wrote to the history file
+# custom arguments
